82a5103d000728fdfcd66b497f223f49dcc35579
Gabriel Lima <gabrielpk1@live.com>
2020-11-15 19:36:33 -0300

    change getValue function and add calculator function

--- script.js
+++ script.js
@@ -5,56 +5,80 @@ let value = [''];
 const add = (a, b) =>  +a + +b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+    if(b == 0){
+        display.style.color = 'red';
+        return "Can't divide by 0";
+     }
+     return  a / b;
+};
 const percent = (a, b) => a * b / 100;
 
 function clearDisplay(){
     Array.from(display.children).forEach(element => (element.nodeName === 'SPAN') ? element.innerText = '': element.innerText = '0' );
+    display.style.color = 'rgb(223, 223, 223)';
+    return [''];
 }
 
 function operate(operator, a, b){
-    console.log(operator, a, b);
     switch(operator){
         case '+':
-            return [add(a, b).toString()];
+            return add(a, b);
         case '−':
-            return [subtract(a, b).toString()];
+            return subtract(a, b);
         case '×':
-            return [multiply(a, b).toString()];
+            return multiply(a, b);
         case '÷':
-            return [divide(a, b).toString()];
+            return divide(a, b);
         case '%':
-            return [percent(a, b).toString()];
+            return percent(a, b);
         case 'Clear':
-            clearDisplay(operator);
-            return [''];
+            return clearDisplay();
         default:
             Array.from(display.children).forEach(element => element.innerText = 'ERROR');
-            display.style.color = 'red';
             break;
 
     }
 }
 
 function getValue(){
-    const operation = document.querySelector('#operation');
-    const result = document.querySelector('#value');
-    if( this.textContent.match(/[^0-9.]/g) || value[(value.length - 1)].match(/[^0-9.]/g) ){
-        value.push(this.textContent);
+    display.style.color = 'rgb(223, 223, 223)';
+    let total = document.querySelector('#value');
+    let operation = document.querySelector('#operation');
+    if(this.textContent === '='){
+        operation.textContent = value.join(' ');
+        calculator();
+        return;
+    }
+    if(this.textContent === 'Clear'){
+        operate(this.textContent);
+        value = [''];
+        return;
+    }
+    if( this.textContent.match(/[^0-9.←]/g) || value[value.length - 1].match(/[^0-9.←]/g) ){
+        if(!(this.textContent === value[value.length - 1])){
+            value.push(this.textContent);
+            operation.textContent = value.join(' ');
+            if(value[value.length - 1].match(/[0-9.]/g) ){
+                total.textContent = value[value.length - 1];
+            }
+        }
     }
     else{
-        value[value.length - 1] += this.textContent;
-        result.textContent = value[value.length - 1];
+        if(this.textContent === '←'){
+            value.pop();
+            if(value.length < 1){
+                value = [''];
+            }
+            total.textContent = '';
+            operation.textContent = value.join(' ');
+            return;
+        }it
+        if(!value[value.length - 1].includes('.') || this.textContent !== '.'){
+            value[value.length - 1] += this.textContent;
+            total.textContent = value[value.length - 1];
+        }
     }
-    if( value.includes('Clear') ){
-        value = operate(value[value.indexOf('Clear')]);
-        result.textContent = 0;
-    }
-    else if(value.length > 3){
-        value = operate(value[1], value[0], value[2]);
-        result.textContent = value;
-    }
-    operation.textContent = value.join(' ');
 }
 
 function getBackGround(){
@@ -66,6 +90,22 @@ function getBackGround(){
                                 , rgba(${red}, ${green}, ${blue}, 0.3))`;
 }
 
-getBackGround();
+function calculator(){
+    let total = document.querySelector('#value');
+    let operation = document.querySelector('#operation');
+    let result = operate(value[1], value[0], value[2]);
+    for(let i = 3; i < value.length-1; i += 2){
+        result = operate(value[i], result, value[i+1]);
+    }
+    if(typeof result === 'number'){
+        value = [Math.round(result * 100) / 100];
+        total.textContent = value;
+        operation.textContent = value;
+    }
+    else{
+        total.textContent = result;
+    }
+}
 
+getBackGround();
 buttons.forEach( button => button.addEventListener('click', getValue));
